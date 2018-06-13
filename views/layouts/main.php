1b14532c42fb619501ea00412420c0ea93fd2021
sparklego <sparklego@163.com>
2018-06-13 18:52:03 +0800

    update files

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Homes', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
