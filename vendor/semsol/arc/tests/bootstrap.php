0f45c6de0e962e31378b9c0914dc16c545e86aae
sparklego <sparklego@163.com>
2018-06-14 23:23:48 +0800

    add ARC lib

---
+++ vendor/semsol/arc/tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ .'/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+require 'ARC2_TestHandler.php';
+
+global $dbConfig;
+
+if (file_exists(__DIR__ .'/config.php')) {
+    // use custom DB credentials, if available
+    $dbConfig = require 'config.php';
+
+} else {
+    // standard DB credentials (ready to use in Travis)
+    $dbConfig = array(
+        'db_name' => 'testdb',
+        'db_user' => 'root',
+        'db_pwd'  => '',
+        'db_host' => 'localhost',
+    );
+}
