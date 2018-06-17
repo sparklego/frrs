ec045890bdeb8c655e00697c295b2e12bb5669e5
sparklego <sparklego@163.com>
2018-06-17 16:34:16 +0800

    add highcharts js lib

---
+++ web/highcharts/code/modules/overlapping-datalabels.js
@@ -0,0 +1,8 @@
+/*
+ Highcharts JS v6.1.0 (2018-04-13)
+
+ (c) 2009-2017 Torstein Honsi
+
+ License: www.highcharts.com/license
+*/
+(function(a){"object"===typeof module&&module.exports?module.exports=a:a(Highcharts)})(function(a){});
---
+++ web/highcharts/code/modules/overlapping-datalabels.src.js
@@ -0,0 +1,17 @@
+/**
+ * @license Highcharts JS v6.1.0 (2018-04-13)
+ *
+ * (c) 2009-2017 Torstein Honsi
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+(function (factory) {
+	if (typeof module === 'object' && module.exports) {
+		module.exports = factory;
+	} else {
+		factory(Highcharts);
+	}
+}(function (Highcharts) {
+
+}));
